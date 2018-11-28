5558c1f765921e8f13e67f5750700ca8660fab8c
Farooq <farooq.0346@hotmail.com>
2018-11-28 05:02:33 +0500

    playing with rules

--- SwiftLintDemo/SwiftLintDemo/Controllers/ViewController.swift
+++ SwiftLintDemo/SwiftLintDemo/Controllers/ViewController.swift
@@ -9,17 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // swiftlint:disable private_outle
     @IBOutlet weak var bottomView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     func awa() -> Int {
         if 5 > 6 {
             print("asdasd")
